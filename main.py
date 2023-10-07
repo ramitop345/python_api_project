af4d395d7f4c607917d8dac40a50df78473063e0
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-10-07 13:01:56 +0200

    querying specific elements with id

--- main.py
+++ main.py
@@ -8,20 +8,34 @@ app = FastAPI()
 
 # it is used for validation when requesting datafrom databank
 class Post(BaseModel):
+    id: int
     title: str
     content: str
     published: bool = True
     rating: Optional[int] = None
 
-@app.post("/createposts/")
-async def create_posts(new_post: Post):
-    print(new_post.title)
+my_posts = []
 
-    print(new_post.model_dump())
-    return {"data": new_post}
-    
 
+def find_post_by_id(id):
+    result = [p for p in my_posts if p["id"] == id]
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/posts/")
+async def create_posts(post: Post):
+    print(post.model_dump())
+    return {"data": post}
+
+@app.get("/posts/")
+async def get_posts():
+    return {"data": my_posts}
+
+#the id is always a string so manually convert in to int if needed
+@app.get("/posts/{id}")
+async def get_post(id: int):
+    post = find_post_by_id(id)
+    return {"Post details": post}
+    
