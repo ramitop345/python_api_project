e9e8edbebc1aa2684f66b71d651fbf2739d7532e
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-10-09 19:47:16 +0200

    modified get_current_user in oauth2.py to return the current user and not just the id

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,14 +1,15 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_scheme= OAuth2PasswordBearer(tokenUrl='login')
 
 SECRET_KEY = "ghfgh5fghfg65jf65f65jf65jfgfg5h5grteg6hg6f5n655fhf6g5h"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_TIME = 30
+ACCESS_TOKEN_EXPIRE_TIME = 60
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -33,6 +34,9 @@ def verify_access_token(token:str, credentials_exception):
     return token_data 
 
 
-def get_current_user(token:str = Depends(oauth2_scheme)):
+def get_current_user(token:str = Depends(oauth2_scheme),db:Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = f"Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
-    return verify_access_token(token, credentials_exception)
+    
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
