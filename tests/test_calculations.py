f3bae25ce8c4defb5dccb877f8577eb75d01d35c
Ramses Kouetsa Kentsa <ramseskouetsa@yahoo.com>
2023-10-13 12:36:37 +0200

    added decoration class from pytest to test module to generate test parameters

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,6 @@
+import pytest
 from  app.calculations import *
 
-def test_add():
-    sum = add(3,2)
-    assert sum == 2
+@pytest.mark.parametrize("num1, num2, expected", [(2,3,5),(0,0,0),(1,1,2)])
+def test_add(num1, num2, expected):
+    assert add(num1,num2) == expected
